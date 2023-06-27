ac52a2c2ca19660a7a8bd6e3526b46ed59206cab
0xJxri <103562889+0xJxri@users.noreply.github.com>
2023-06-27 22:53:00 +0200

    Almost end commit

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-var imageContainer = document.getElementById('print');
-var table = document.getElementById('table');
+var table = document.getElementById("table");
 var teamCount = 0;
+const storeNames = []; 
+var arrayCells = ["first", "second", "third", "fourth", "fifth", "sixth"];
 
 //API call
 function pokePhoto() {
-  var input = document.getElementById('inputPokemon').value;
+  var input = document.getElementById("inputPokemon").value;
 
-  fetch("https://pokeapi.co/api/v2/pokemon/"+input)
+  fetch("https://pokeapi.co/api/v2/pokemon/" + input)
     .then((response) => {
       return response.json();
     })
     .then((res) => {
       var name = res.name;
-      var sprite = res.sprites.other["official-artwork"].front_default;
+      var photo = res.sprites.other["official-artwork"].front_default;
 
-      // Update table with Pokemon name
+      console.log(photo);
+
+      if (storeNames.length < 6) {
+        storeNames.push(name);
+        console.log(storeNames);
+      }
+
       if (teamCount < 6) {
         var cell = table.rows[1].cells[teamCount];
         cell.innerHTML = name;
@@ -25,41 +32,87 @@ function pokePhoto() {
     })
     .catch((error) => {
       document.getElementById("error").innerHTML = "Pokemon not found!";
-        
+
       return error;
     });
 }
 
-//Delete last child
-function deleteChild(){
-  // Clear the last Pokemon name in the table
+function deleteChild() {
   if (teamCount > 0) {
     teamCount--;
     var cell = table.rows[1].cells[teamCount];
-    cell.innerHTML = "empty";
+    cell.innerHTML = "//";
+    storeNames.pop();
+  }
+
+  var lastSlideId = arrayCells[arrayCells.length - 1];
+  var lastSlide = document.getElementById(lastSlideId);
+  lastSlide.remove();
+  arrayCells.pop();
+}
+
+async function createTeam() {
+  for (var i = 0; i < 6; i++) {
+    var res, pathPhoto, img;
+
+    await fetch("https://pokeapi.co/api/v2/pokemon/" + storeNames[i])
+      .then((res) => res.json())
+      .then((response) => {
+        res = response;
+        pathPhoto = res.sprites.other["official-artwork"].front_default;
+        console.log(pathPhoto);
+
+        img = new Image();
+        img.src = pathPhoto;
+
+        return new Promise((resolve) => {
+          img.onload = resolve;
+        });
+      })
+      .then(() => {
+        document.getElementById(arrayCells[i]).appendChild(img);
+        document.getElementById("error").innerHTML = " ";
+      })
+      .catch((error) => {
+        document.getElementById("error").innerHTML = "Pokemon not found!";
+        console.error(error);
+      });
   }
 }
 
-// Bosses button click event
-document.getElementById("bosses").addEventListener("click", function(event) {
-    if (teamCount < 6) {
-        event.preventDefault(); // Interrompe l'azione predefinita del collegamento
-        alert("You need a team of 6 to challenge the champions!");
-    } else {
-      // Perform the desired action when the team is complete
-      window.location.href = "bosses.html";
-    }
-  });
-  // Healer button click event
-document.getElementById("healer").addEventListener("click", function(event) {
-    if (teamCount === 0) {
-      event.preventDefault(); // Interrompe l'azione predefinita del collegamento
-      alert("You can't see your team without creating an actual one!");
-    } else {
-      // Perform the desired action when the team is created
-      // E.g., redirect to the healer page
-      window.location.href = "healer.html";
-    }
-  });
+document.getElementById("bosses").addEventListener("click", function (event) {
+  if (teamCount < 6) {
+    event.preventDefault(); // Interrompe l'azione predefinita del collegamento
+    alert("You need a team of 6 to challenge the champions!");
+  } else {
+    window.location.href = "bosses.html";
+  }
+});
+document.getElementById("healer").addEventListener("click", function (event) {
+  if (teamCount === 0) {
+    event.preventDefault(); // Interrompe l'azione predefinita del collegamento
+    alert("You can't see your team without creating an actual one!");
+  } else {
+    window.location.href = "healer.html";
+  }
+});
 
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
 
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
